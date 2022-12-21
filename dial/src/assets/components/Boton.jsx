fcaae7e5a6b842ef6690c9979bcbc428b7e703ea
jordislpm <jordislpm@gmail.com>
2022-12-21 18:28:01 -0400

    listo

---
+++ dial/src/assets/components/Boton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Boton(props){
+    return (
+        <>
+        <button onClick={props.botonClick}>Stop/Start</button>
+        </>
+    )
+}
+
+
+
+export default Boton
