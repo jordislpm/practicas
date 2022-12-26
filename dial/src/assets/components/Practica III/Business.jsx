ee389e938ab98fd000b5fd00207e327e72ee3b31
jordislpm <jordislpm@gmail.com>
2022-12-26 14:04:11 -0400

    practica 3 primer version completa

--- dial/src/assets/components/Practica III/Business.jsx
+++ dial/src/assets/components/Practica III/Business.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import Boton from './Boton';
 
 
-function Business (props){
+export default function Business (props){
 return(
     <div>
     <h2>{props.name}</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ipsum reprehenderit labore totam tempore nesciunt est voluptatem aliquid, dolore cupiditate error consequatur qui pariatur ab incidunt quo ea quod officiis?</p>
+    <p>{props.content}</p>
     <Boton name="Call to Action!"/>
     </div>
 )
 }
 
-export default Business;
