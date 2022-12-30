6dac2ddb5213c91e2b1430d2ccd05a94561f47de
jordislpm <jordislpm@gmail.com>
2022-12-29 23:56:56 -0400

    practica 4 casi lista

--- dial/src/App.jsx
+++ dial/src/App.jsx
@@ -1,10 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MenuBar from './assets/components/Practica III/MenuBar';
 import Secction1 from './assets/components/Practica III/Secction1';
 import Secction2 from './assets/components/Practica III/Secction2';
 import Secction3 from './assets/components/Practica III/Secction3';
 import Footer from './assets/components/Practica III/Footer';
+
+import Form from './assets/components/Practica IV/Form';
+import Tabla from './assets/components/Practica IV/Tabla';
+
 
 
 
@@ -51,20 +55,80 @@ export default class App extends Component{
         {
           url: "https://www.bgcdc.org/wordpress/wp-content/uploads/2020/08/900x400.png"
         }
-      ]
+      ],
+      values:["hola"],
+      monto:0,
+      interesAnual:0,
+      cuota:0
       
     }
-  }
-  render(){
-    return <div clasName="showtime">
-    <MenuBar name="Jordis.dev"/>
-    <div className='container px-8'>
-    <Secction1 businessData={this.state.businessData[0]} img={this.state.img[0]}/>
-    <Secction2/>
-    <Secction3 cards={this.state.cards}/>
-    </div>
-    <Footer/>
+  };
 
+
+  createMes = (e,monto,interesAnual,cuota)=>{
+  
+    e.preventDefault();
+    let V = monto;
+    let N = cuota
+    let I = (interesAnual/12)/100;
+    let interesMensual = (V * I);
+    let C = (interesMensual/ (1-(1 + I)**(N * -1))).toFixed(2)
+    let porcMensual = interesMensual /100;
+    let capital = C - interesMensual;
+    let balance = V - capital;
+    let interes = interesMensual;
+    const interesPorc = interesMensual / 100;
+
+    for (let i = 0; i < cuota; i++) {
+
+
+
+        const mes ={
+            Mes:`${i+1}`,
+            Cuota: `${C}`,
+            capital:`${capital}`,
+            Interes:`${interes}`,
+            Balance: `${balance}`
+        
+        };
+
+        this.setState({values:[mes]})
+        interes = (balance * interesPorc/100).toFixed(2);
+        capital = (C - interes).toFixed(2);
+        balance = (balance - capital).toFixed(2);
+    }
+   
+    console.log(this.state.values)
+}
+
+  handleMonto = (e)=>{
+    this.setState({monto:e.target.value})
+  };
+
+
+  handleInteresAnual = (e)=>{
+    this.setState({interesAnual:e.target.value})
+  };
+
+  handleCuota = (e)=>{
+    this.setState({cuota:e.target.value})
+  };
+
+  render(){
+    return <div>
+    <h2>calculadora financiera de cuota de prestamos</h2>
+    <Form 
+    cuota={this.state.cuota}
+    monto={this.state.monto} 
+    interesAnual={this.state.interesAnual}
+    createMes={this.createMes}
+    handleMonto={this.handleMonto} 
+    handleCuota={this.handleCuota}
+    handleInteresAnual={this.handleInteresAnual}
+   
+    />
+    <Tabla meses={meses}/>
+    
     </div>
   }
 }
--- dial/src/assets/components/Practica III/Secction3.jsx
+++ dial/src/assets/components/Practica III/Secction3.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import Card from './card';
-import Img from './Img';
 
 export default function Secction3(props) {
     return(
     <div className="row d-flex justify-content-between">
         {props.cards.map((card, i)=>{
             return  <div className="col m-4 border border-secondary">
-            <Card key={`${card}${i}`} name={card.name} content={card.content}/>
+            <Card key={`${i}`} name={card.name} content={card.content}/>
             </div>
         })}
     </div>
