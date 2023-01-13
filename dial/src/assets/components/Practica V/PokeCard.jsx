0622618f331b35f9f7e737ae7c3746d844ff8887
jordislpm <jordislpm@gmail.com>
2023-01-13 17:12:58 -0400

    modal listo

--- dial/src/assets/components/Practica V/PokeCard.jsx
+++ dial/src/assets/components/Practica V/PokeCard.jsx
@@ -6,7 +6,7 @@ export default function PokeCard (props){
             <>
 				<div className="card-info">
 					<span className="pokemon-id">No.{props.id}</span>
-					<h3>{props.name}</h3>
+					<h3>{(props.name).charAt(0).toUpperCase() + (props.name).slice(1)}</h3>
 					<div className="card-types">
 					</div>
 				</div>
